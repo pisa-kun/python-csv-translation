e8ea03d1d31d2964bb8f5e0d9d68ace105200317
pisa <pisa0401mst@gmail.com>
2022-11-09 04:24:19 +0900

    csvのヘッダー行から空白を削除、tocsvのindexを無視する

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import mojimoji
 
 file = 'src\\test_0001_20221109.csv'
 df = pd.read_csv(file, header=0)
@@ -20,8 +21,13 @@ df.insert(0, "format", f)
 df.insert(1, "time", t) 
 
 # CF, FZのみ抜き出し
-isModel = df["serial"].str.startswith(("CF", "FZ"))
-print(df[isModel])
+isModel = df['serial'].str.startswith(("CF", "FZ"))
+df2 = df[isModel]
+print(df2)
 
-#print(df)
-#df.to_csv("dist\\test_20221109.csv")
+# memoフィールドの英数字を全角から半角に
+memo = df2["memo"]
+print(memo)
+
+# df型は列番号をもつので、indexは無視する
+df2.to_csv("dist\\test_20221109.csv", index=False)
