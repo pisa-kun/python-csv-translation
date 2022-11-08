6056d7d3d2f49fc863ba3d3f2a6b57ac2c1258eb
pisa <pisa0401mst@gmail.com>
2022-11-09 03:16:17 +0900

    列の追加

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('src\\test_0001_20221109.csv', header=0)
 print(df)
+
+# フォーマットの追加
+df["format"] = "1000"
+# 日付の追加
+df["time"] = "20220425"
+print(df)
+#df.to_csv("dist\\test_20221109.csv")
