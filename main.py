f253190e2bb9109fd4e7ba88eb707b65fa34119f
pisa <pisa0401mst@gmail.com>
2022-11-09 03:25:46 +0900

    insertに変更

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ df = pd.read_csv('src\\test_0001_20221109.csv', header=0)
 print(df)
 
 # フォーマットの追加
-df["format"] = "1000"
+# 先頭に追加
+df.insert(0, "format", 1000)
 # 日付の追加
-df["time"] = "20220425"
+# 2行目に追加
+df.insert(1, "time", "20220425") 
 print(df)
 #df.to_csv("dist\\test_20221109.csv")
