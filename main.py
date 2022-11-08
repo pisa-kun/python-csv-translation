329dcee491228915217dfbadfc99ce3e625d1c8d
pisa <pisa0401mst@gmail.com>
2022-11-09 03:48:57 +0900

    正規表現処理追加

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import re
 
-df = pd.read_csv('src\\test_0001_20221109.csv', header=0)
+file = 'src\\test_0001_20221109.csv'
+df = pd.read_csv(file, header=0)
 print(df)
+
+# searchできない場合はm.groupで例外発生
+m = re.search(r'\d{4}', file)
+f = m.group()
+
+m2 = re.search(r'\d{8}', file)
+t = m2.group()
 
 # フォーマットの追加
 # 先頭に追加
-df.insert(0, "format", 1000)
+df.insert(0, "format", f)
 # 日付の追加
 # 2行目に追加
-df.insert(1, "time", "20220425") 
+df.insert(1, "time", t) 
 print(df)
 #df.to_csv("dist\\test_20221109.csv")
